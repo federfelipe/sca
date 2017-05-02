4e28d1b19154f4d86fe1d54b58de277f6b534bce
Luiz <Luiz@lenovo>
2017-05-02 20:26:00 -0300

    Classe Alocacao Horario

---
+++ src/main/java/br/cefetrj/sca/dominio/AlocacaoHorario.java
@@ -0,0 +1,54 @@
+package br.cefetrj.sca.dominio;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class AlocacaoHorario {
+	
+	private Long id;
+	
+	private Turma turmaId;
+	private ItemHorario itemHorarioInicio;
+	private ItemHorario itemHorarioFim;
+	
+	@SuppressWarnings("unused")
+	private AlocacaoHorario() {
+	}
+	
+	public AlocacaoHorario( Turma turmaId, ItemHorario itemHorarioInicio, ItemHorario itemHorarioFim ) {
+		this.turmaId = turmaId;
+		this.itemHorarioInicio = itemHorarioInicio;
+		this.itemHorarioFim = itemHorarioFim;
+	}
+	
+	public Long getId() {
+		return this.id;
+	}
+	
+	public Turma getTurmaId() {
+		return this.turmaId;
+	}
+	
+	public ItemHorario getItemHorarioInicio() {
+		return this.itemHorarioInicio;
+	}
+	
+	public ItemHorario getItemHorarioFim() {
+		return this.itemHorarioFim;
+	}
+	
+	public void setTurmaId( Turma turmaId ) {
+		this.turmaId = turmaId;
+	}
+	
+	public void setItemHorarioInicio( ItemHorario itemHorarioInicio ) {
+		this.itemHorarioInicio = itemHorarioInicio;
+	}
+	
+	public void setItemHorarioFim( ItemHorario itemHorarioFim ) {
+		this.itemHorarioFim = itemHorarioFim;
+	}
+	
+
+}

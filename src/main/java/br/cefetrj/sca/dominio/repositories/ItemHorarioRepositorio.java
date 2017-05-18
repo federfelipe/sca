a0817789e6d131522dde6265d674aa5e000094a7
Luiz <Luiz@lenovo>
2017-05-18 20:59:00 -0300

    Repositorios ItemHorario e Turma

--- src/main/java/br/cefetrj/sca/dominio/repositories/ItemHorarioRepositorio.java
+++ src/main/java/br/cefetrj/sca/dominio/repositories/ItemHorarioRepositorio.java
@@ -1,27 +1,33 @@
 package br.cefetrj.sca.dominio.repositories;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import br.cefetrj.sca.dominio.ItemHorario;
-import br.cefetrj.sca.dominio.avaliacaoturma.AvaliacaoTurma;
 
 public interface ItemHorarioRepositorio extends
-JpaRepository<AvaliacaoTurma, Serializable> {
+JpaRepository<ItemHorario, Serializable> {
 	
-	@Query("SELECT item FROM itemhorario item WHERE item.id = ?1")
+	@Query("SELECT ih FROM itemhorario ih WHERE ih.id = ?1")
 	public ItemHorario getItemHorario(Long id);
 	
-	@Query("SELECT item FROM itemhorario item WHERE item.dia_semana = ?1")
+	@Query("SELECT ih FROM itemhorario ih WHERE ih.dia_semana = ?1")
 	public List<ItemHorario> findItensHorariosByDiaSemana(String dia);
 	
-	@Query("SELECT item FROM itemhorario item WHERE item.tempo = ?1")
+	@Query("SELECT ih FROM itemhorario ih WHERE ih.tempo = ?1")
 	public List<ItemHorario> findItensHorariosByTempo(int tempo);
 	
-	@Query("SELECT item FROM itemhorario item WHERE item.turno = ?1")
+	@Query("SELECT ih FROM itemhorario ih WHERE ih.turno = ?1")
 	public List<ItemHorario> findItensHorariosByTurno(int turno);
+	
+	@Query("SELECT ih FROM itemhorario ih WHERE ih.hora_inicio = ?1")
+	public List<ItemHorario> findItemHorariosByHoraInicio(Date horaInicio);
+	
+	@Query("SELECT ih FROM itemhorario ih WHERE ih.hora_fim = ?1")
+	public List<ItemHorario> findItemHorariosByHoraFim(Date horaFim);
 
 }

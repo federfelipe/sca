fd54f98e0d92d85425890b0cf3480c0bf66b78d9
FederFelipe <felipeoliveirafeder@hotmail.com>
2017-05-19 01:12:12 -0300

    restauração ItemHorario

--- src/main/java/br/cefetrj/sca/dominio/ItemHorario.java
+++ src/main/java/br/cefetrj/sca/dominio/ItemHorario.java
@@ -1,16 +1,11 @@
 package br.cefetrj.sca.dominio;
 
-import java.sql.SQLException;
-import java.sql.Time;
-
-//import javax.persistence.Embedded;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
-//import javax.persistence.EnumType;
-//import javax.persistence.Enumerated;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
-import com.mysql.jdbc.Statement;
 
 @Entity
 public class ItemHorario {
@@ -18,52 +13,7 @@ public class ItemHorario {
 	@GeneratedValue
 	private Long id;
 
-	private int dia_semana;
-	
-	private Time hora_inicio;
-	
-	private Time hora_fim;
-	
-	private int tempo;
-	
-	private int turno;
-	
-	public static void main(String[] args) throws SQLException {
-        System.out.println("Hello World");
-        insertion();
-        
-     }
-	
-	public static void insertion() throws SQLException {
-          Statement stmt = null;
-          try {
-              stmt = con.createStatement();
-              stmt.executeUpdate(
-                  "insert into " + "scadb" +
-                  ".itemhorario " +
-                  "values(2, 'felipe', 'diogo', 11, 22)");
-
-              /*stmt.executeUpdate(
-                  "insert into " + dbName +
-                  ".COFFEES " +
-                  "values('French_Roast', " +
-                  "00049, 8.99, 0, 0)");*/
-              
-          } catch (SQLException e) {
-              printSQLException(e);
-          } finally {
-              if (stmt != null) {
-                stmt.close();
-              }
-          }
-      }
-
-	private static void printSQLException(SQLException e) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-/*	@Embedded
+	@Embedded
 	private IntervaloTemporal tempoAula;
 
 	@Enumerated(EnumType.ORDINAL)
@@ -92,5 +42,5 @@ public class ItemHorario {
 	
 	public boolean colide(ItemHorario item) {
 		return this.tempoAula.colide(item.tempoAula);
-	}*/
+	}
 }

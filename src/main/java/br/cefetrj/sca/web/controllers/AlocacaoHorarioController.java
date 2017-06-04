de1b98f455d5fd8bbd99e1827bdbc00e10b6a9d7
Luiz <Luiz@lenovo>
2017-06-03 21:16:14 -0300

    Metodo para carregar dados da base e auxiliar na montagem da tabela

--- src/main/java/br/cefetrj/sca/web/controllers/AlocacaoHorarioController.java
+++ src/main/java/br/cefetrj/sca/web/controllers/AlocacaoHorarioController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import br.cefetrj.sca.dominio.Disciplina;
+import br.cefetrj.sca.dominio.Professor;
 import br.cefetrj.sca.dominio.Turma;
 import br.cefetrj.sca.service.AlocacaoHorarioService;
 
@@ -22,7 +24,7 @@ public class AlocacaoHorarioController {
 	protected Logger logger = Logger.getLogger(AvaliacaoTurmaController.class.getName());
 	
 	@Autowired
-	private AlocacaoHorarioService alocahorarioservice; 
+	private AlocacaoHorarioService alocaHorarioService; 
 
 	@RequestMapping(value = "/{*}", method = RequestMethod.GET)
 	public String get(Model model) {
@@ -30,11 +32,37 @@ public class AlocacaoHorarioController {
 		return "/homeView";
 	}
 	
+	@RequestMapping(value = "/menuPrincipal", method = RequestMethod.GET)
+	public String retornaMenuPrincipal() {
+		return "/menuPrincipalView";
+	}
 	
+	@RequestMapping(value = "/montarTabela", method = RequestMethod.GET)
+	public String exibeMontarTabela(Model model) {
+
+		List<Turma> turmas = alocaHorarioService.findAllTurmas();
+		List<Disciplina> disciplinas = alocaHorarioService.findAllDisciplinas();
+		List<Professor> professores = alocaHorarioService.findAllProfessores();
+//		List<String> horasInicio = alocaHorarioService.getAllHorasInicio();
+//		List<String> horasFim = alocaHorarioService.getAllHorasFim();
+		List<String> diasSemana = alocaHorarioService.getAllDiasSemana();
+		
+		model.addAttribute("turmas", turmas);
+		model.addAttribute("disciplinas", disciplinas);
+		model.addAttribute("professores", professores);
+//		model.addAttribute("horasInicio", horasInicio);
+//		model.addAttribute("horasFim", horasFim);
+		model.addAttribute("diasSemana", diasSemana);
+		
+		return "/alocacaoHorario/alocacaoHorarioView";
+		
+	}
+	
+	// Metodo usado antes apenas para teste. Depois sera removido
 	@RequestMapping(value = "/definirTabela")
 	public String exibeMontarTabela(ModelMap model) {
 		
-		List<Turma> turmas = alocahorarioservice.findAll();
+		List<Turma> turmas = alocaHorarioService.findAll();
 		model.addAttribute("turmas", turmas);
 
 		return "/alocacaoHorarioTurma/alocacaoHorarioView";

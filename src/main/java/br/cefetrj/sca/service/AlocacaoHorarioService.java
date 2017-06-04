4b418d99dbe8e5c0c1e1d424eed347d797b67bb5
Luiz <Luiz@lenovo>
2017-06-03 21:16:45 -0300

    Metodos de acesso a base

--- src/main/java/br/cefetrj/sca/service/AlocacaoHorarioService.java
+++ src/main/java/br/cefetrj/sca/service/AlocacaoHorarioService.java
@@ -2,17 +2,17 @@ package br.cefetrj.sca.service;
 
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.cefetrj.sca.dominio.Disciplina;
+import br.cefetrj.sca.dominio.EnumDiaSemana;
 import br.cefetrj.sca.dominio.ItemHorario;
+import br.cefetrj.sca.dominio.Professor;
 import br.cefetrj.sca.dominio.Turma;
-import br.cefetrj.sca.dominio.repositories.DisciplinaRepositorio;
 import br.cefetrj.sca.dominio.repositories.ItemHorarioRepositorio;
-import br.cefetrj.sca.dominio.repositories.LocalAulaRepositorio;
 import br.cefetrj.sca.dominio.repositories.ProfessorRepositorio;
 import br.cefetrj.sca.dominio.repositories.TurmaRepositorio;
 
@@ -25,15 +25,7 @@ public class AlocacaoHorarioService {
 	
 	private ItemHorarioRepositorio itemHorarioRepositorio;
 	
-	private DisciplinaRepositorio disciplinaRepositorio;
-	
-	private ProfessorRepositorio professorController;
-	
-	private LocalAulaRepositorio localAulaRepositorio;
-	
-	private ItemHorario itemHorarioInicio;
-	
-	private ItemHorario itemHorarioFim;
+	private ProfessorRepositorio professorRepositorio;
 	
 	// # estou usando esse método #
 	public List<Turma> findAll() {
@@ -46,22 +38,62 @@ public class AlocacaoHorarioService {
 		return itensHorario;
 	}
 	
-	public List<Date> getHorariosInicio(List<ItemHorario> itensHorario) {
-		List<Date> horariosInicio = new ArrayList<Date>();
-		for(ItemHorario itemHorario : itensHorario) {
-		/*	Date horaInicio = itemHorario.getHoraInicio();
-			horariosInicio.add(horaInicio);*/
-		}
-		return horariosInicio;
+	
+	// O metodo abaixo faz a mesma coisa que o metodo findAll() acima, porem possui um nome mais claro
+	// Metodo adicionado
+	public List<Turma> findAllTurmas() {
+		return turmaRepositorio.findAll();
 	}
 	
-	public List<Date> getHorariosFim(List<ItemHorario> itensHorario) {
-		List<Date> horariosFim = new ArrayList<Date>();
-		for(ItemHorario itemHorario : itensHorario) {
-		/*	Date horaFim = itemHorario.getHoraFim();
-			horariosFim.add(horaFim);*/
+	// Metodo adicionado
+	public List<Disciplina> findAllDisciplinas() {
+		List<Turma> turmas = findAllTurmas();
+		List<Disciplina> disciplinas = new ArrayList<Disciplina>();
+		for(Turma turma : turmas)
+		{
+			Disciplina disciplina = turma.getDisciplina();
+			disciplinas.add(disciplina);
 		}
-		return horariosFim;
+		return disciplinas;
+	}
+	
+	// Metodo adicionado
+	public List<Professor> findAllProfessores() {
+		return professorRepositorio.findAll();
+	}
+	
+	// Metodo adicionado
+	public List<ItemHorario> findAllItensHorario() {
+		return itemHorarioRepositorio.findAll();
+	}
+	
+	// Metodo adicionado
+	public List<String> getAllHorasInicio() {
+		List<String> horasInicio = new ArrayList<String>();
+		List<ItemHorario> itensHorario = itemHorarioRepositorio.findAll();
+		for(ItemHorario itemHorario : itensHorario)
+		{
+			String horaInicio = itemHorario.getInicio();
+			horasInicio.add(horaInicio);
+		}
+		return horasInicio;
+	}
+	
+	// Metodo adicionado
+	public List<String> getAllHorasFim() {
+		List<String> horasFim = new ArrayList<String>();
+		List<ItemHorario> itensHorario = itemHorarioRepositorio.findAll();
+		for(ItemHorario itemHorario : itensHorario)
+		{
+			String horaFim = itemHorario.getFim();
+			horasFim.add(horaFim);
+		}
+		return horasFim;
+	}
+	
+	// Metodo adicionado
+	public List<String> getAllDiasSemana() {
+		return EnumDiaSemana.dias();
 	}
 
 }
